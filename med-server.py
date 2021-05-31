66adc807cd7638a06bfbc31802b23994a907f1a9
lovelesh patel <lovelesh06101990@gmail.com>
2021-06-01 01:39:19 +0530

    med-server functionality completed
    - added connection to mongodb
    - added mechanism to push data to db
    - functionality tested

--- med-server.py
+++ med-server.py
@@ -1,36 +1,33 @@
-#code for parsing json file
+# code for parsing json file
 # Using flask to make an api
 # import necessary libraries and functions
+from flask import Flask, request, render_template, jsonify
+from flask_pymongo import PyMongo
+from pymongo.errors import BulkWriteError
 
-from flask import Flask, jsonify, request, render_template
-  
 # creating a Flask app
 app = Flask(__name__)
 # path to the database
-db_path = "<enter your db path here>"
+app.config["MONGO_URI"] = "mongodb://localhost:27017/med_sensor_db"
+mongodb_client = PyMongo(app)
+db = mongodb_client.db
+
 
 # returns simple strings when we use GET.
 # returns the data that we send when we use POST.
 @app.route('/')
 def index():
-   return render_template('index.html')
+    return render_template('index.html')
 
-@app.route('/wearable-testing/data', methods = ['POST'])
+
+@app.route('/wearable-testing/data', methods=['POST'])
 def data():
     content = request.get_json()
     print(content)
+    db.sensor_data.insert_one(content)
+    return 'record added to db'
 
-    return 'JSON received'
 
-
-
-# this returns 100 (square of 10)
-@app.route('/home/<int:num>', methods = ['GET'])
-def disp(num):
-  
-    return jsonify({})
-  
-  
 # driver function
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=80, threaded=True)
